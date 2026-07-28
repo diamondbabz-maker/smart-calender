063ff6184177bd179e1d385fcfcf8416a421119b
diamondbabz-maker <diamondbabzkohinoor@gmail.com>
2026-07-27 23:32:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,62 +4,39 @@
 
 let slideShowRunning = true;
 
-let slideTimer =
-    setInterval(nextPhoto, 5000);
+let slideTimer = setInterval(nextPhoto, 5000);
 
-function nextPhoto(){
-
+function nextPhoto() {
     currentPhoto++;
 
-    if(currentPhoto >= photos.length){
-
+    if (currentPhoto >= photos.length) {
         currentPhoto = 0;
-
     }
 
-    document.getElementById("photo").src =
-        photos[currentPhoto];
-
+    document.getElementById("photo").src = photos[currentPhoto];
 }
 
-function previousPhoto(){
-
+function previousPhoto() {
     currentPhoto--;
 
-    if(currentPhoto < 0){
-
+    if (currentPhoto < 0) {
         currentPhoto = photos.length - 1;
-
     }
 
-    document.getElementById("photo").src =
-        photos[currentPhoto];
-
+    document.getElementById("photo").src = photos[currentPhoto];
 }
 
-function toggleSlideshow(){
-
-    if(slideShowRunning){
-
+function toggleSlideshow() {
+    if (slideShowRunning) {
         clearInterval(slideTimer);
-
         slideShowRunning = false;
-
-        document.getElementById("playPause").textContent =
-            "Play";
-
-    }else{
-
-        slideTimer =
-            setInterval(nextPhoto,5000);
-
+        document.getElementById("playPause").textContent = "Play";
+    } else {
+        slideTimer = setInterval(nextPhoto, 5000);
         slideShowRunning = true;
-
-        document.getElementById("playPause").textContent =
-            "Pause";
-
+        document.getElementById("playPause").textContent = "Pause";
     }
-
 }
+
 // Slideshow starts automatically.
 // No second timer needed here.
